8de1c3540de29e82064bb8e6074f06873b5f97ad
Shams <49056370+sa05169@users.noreply.github.com>
2020-06-19 23:56:48 +0500

    Update task-08.py

--- task-08.py
+++ task-08.py
@@ -20,7 +20,7 @@ def Pick_from_Circ(Radius):
 
 def Expected_Time(Radius):
     Bots = 500
-    Time = 1000
+    Time = 2000
 
     mybots = list()                     # bots list
     for i in range(Bots):
