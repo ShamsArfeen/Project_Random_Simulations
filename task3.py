55454aa03361e724da94f375f99a7f45450d1f29
Anand Kumar <ak05173@st.habib.edu.pk>
2020-06-19 17:52:41 +0500

    Added Task 3 and Task 4

---
+++ task3.py
@@ -0,0 +1,42 @@
+import random
+import math
+import numpy
+
+""" ----------------------------------- TASK 3 -----------------------------------------
+Create a two-dimensional random walk model using simulation-based approach within boundaries
+of a circular disc. Simulation will be discrete with respect to step-size {0, 0.5, 1} and
+direction [0 ~ 360].
+"""
+
+def Round_Simulation(Radius):
+    Time = 500
+    Bots = 500
+    
+    Step_Size = [0, 0.5, 1]                                     # discrete steps
+    Orientation = [0, math.pi / 2, math.pi, 3 * math.pi / 2]    # discrete orientation
+    
+    Curr_Pos = list()
+    myBot = list()
+    for _ in range(Bots):
+        Curr_Pos.append( (0,0) )                                #Initial position of bots           
+        myBot.append( list() )
+
+    for Step in range(Time):
+        
+        for bot in range(Bots):
+            dr = Step_Size[ random.randint(0,2) ]               #Randomly selects one of the step sizes and orientation
+            dt = Orientation[ random.randint(0, 3) ]
+
+            x, y = Curr_Pos[bot]
+            dx, dy = dr * math.cos(dt), dr * math.sin(dt)       #Converts the movement into cartesian coordinates
+
+            Curr_Pos[bot] = (x + dx, y + dy)
+
+            if (x + dx)**2 + (y + dy)**2 > Radius**2:           #Formula mentioned in the write-up
+                Curr_Pos[bot] = (random.choice([-1,0,1]), random.choice([-1,0,1]))
+
+            myBot[bot].append(Curr_Pos[bot])
+
+    return Curr_Pos
+
+print("Bots are standing on points:", Round_Simulation(100))
