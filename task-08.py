6d258434d4b873253e33e1d466f9a3af0fb20fb5
Shams <49056370+sa05169@users.noreply.github.com>
2020-06-19 22:27:24 +0500

    Update task-08.py

--- task-08.py
+++ task-08.py
@@ -1,7 +1,6 @@
 import random
 import math
 import numpy
-
 """ ----------------------------------- TASK 8 -----------------------------------------
 Building on task 5, each team will capture the trajectory of two nodes whose initial locations are
 chosen randomly and uniformly over a circular region. Every team will be asked to explain how did
@@ -12,7 +11,7 @@ def Distance(P1, P2):
     x1, y1 = P1
     x2, y2 = P2
     return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-    
+
 
 def Pick_from_Circ(Radius):
     dt = 2 * math.pi * numpy.random.uniform(0.0, 1.0)
@@ -20,61 +19,56 @@ def Pick_from_Circ(Radius):
     return (math.cos(dt) * dr, math.sin(dt) * dr)
 
 def Expected_Time(Radius):
-    Bots = 300
-    Time = 300
+    Bots = 500
+    Time = 500
 
-    A = Pick_from_Circ(Radius)
-    B = Pick_from_Circ(Radius)
-
-    print( 'Distance :', Distance(A, B))
-    
-    A_bot = list()                  # bots on point A
+    mybots = list()                     # bots list
     for i in range(Bots):
-        A_bot.append( A )
+        mybots.append( Pick_from_Circ(Radius) )
 
-    B_bot = list()                  # bots on point B
-    for i in range(Bots):
-        B_bot.append( B )
-
-    Lost = list()                   # stores pairs of bots (A,B) which are yet to meet
-    for a in range(Bots):
-        for b in range(Bots):
+    Lost = list()                       # stores pairs of bots (A,B) which are have not yet met.
+                                        # This list will update on every step
+    for a in range(len(mybots) - 1):
+        for b in range(a+1, len(mybots)):
             Lost.append( (a,b) )
 
-    def Take_A_Step(my_Bots):      # simulates given bots by one step
+    def Take_A_Step(my_Bots):           # simulates all given bots by one step
         for bot in range(len(my_Bots)):
             x,y = my_Bots[bot]
-            
+
             dr = numpy.random.uniform(0.0, 1.0)
             dt = 2 * math.pi * numpy.random.uniform(0.0, 1.0)
-            
+
             dx = dr * math.cos(dt)
             dy = dr * math.sin(dt)
 
-            my_Bots[bot] = (x + dx, y + dy)
+            x = x + dx
+            y = y + dy
+            r = math.sqrt(x**2 + y**2)
 
-            if (x + dx)**2 + (y + dy)**2 > Radius**2:           #Formula mentioned in the write-up
-                my_Bots[bot] = ( (x+dx) / (math.sqrt((x+dx)**2 + (y+dy)**2)), (y+dy) / (math.sqrt((x+dx)**2 + (y+dy)**2)))
-            
+            if r > Radius:
+                my_Bots[bot] = (x / r, y / r)
+            else:
+                my_Bots[bot] = (x + dx, y + dy)
+
         return my_Bots
 
     Total_Meets = 1
-    Meetings = [ 0 ]                # Meetings[i] stores no. of meetings on step i
+    Meetings = [ 0 ]                # Meetings[i] stores no. of meetings occured on step i
     for Step in range(Time):
         Meetings.append( 0 )
-        
-        A_bot = Take_A_Step(A_bot)
-        B_bot = Take_A_Step(B_bot)
+
+        mybots = Take_A_Step(mybots)
 
         Restore = list()            # A bot pair (a,b) that has recorded meet will not meet again
         for a, b in Lost:
-            if Distance(A_bot[a], B_bot[b]) < 1:
+            if Distance(mybots[a], mybots[b]) < 1:
                 Meetings[Step] += 1
                 Total_Meets += 1
             else:
                 Restore.append( (a,b) )
         Lost = Restore
-        
+
     return sum([Step * Meetings[Step] for Step in range(Time)]) / Total_Meets
 
-print('Expected time to meet:', Expected_Time(10))
+print(Expected_Time(8))
